319ee8f2c3eb4c0c57f6a87eb19e0c73ae6ad19e
Marcelo Eltz <marcelo@growdev.com.br>
2022-07-20 12:06:49 -0300

    Alterado dev noa mostrar em producao

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,9 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import rootReducer from './modules/rootReducer';
 
-const store = configureStore({ reducer: rootReducer });
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+});
 
 export { store };
